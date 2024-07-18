172aba51fe4391e315c4f1424ba6187b6b9ed4a6
Arthur Zopellaro <github@zope.me>
2024-07-18 16:01:11 -0300

    wip, feat: add cli.apt script, improve gui.apt script

--- scripts/cli/apt.sh
+++ scripts/cli/apt.sh
@@ -1,9 +1,8 @@
-
 #!/bin/bash
 
-APPS="alacritty wavebox"
+APPS="bat"
 
-APP_NAME="apt.gui"
+APP_NAME="apt.cli"
 LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
 echo ">> Logging to $LOG_TMP_FILE"
 date >> $LOG_TMP_FILE
@@ -12,5 +11,4 @@ echo ">> Updating $APP_NAME..." | tee -a $LOG_TMP_FILE
 sudo apt-get update >> $LOG_TMP_FILE
 echo ">> Installing $APPS..." | tee -a $LOG_TMP_FILE
 sudo apt-get install $APPS -y >> $LOG_TMP_FILE
-
 
