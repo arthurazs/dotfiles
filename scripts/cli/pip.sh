e6a02d2c7d8367ffee8280bdfc703f25f7a76fc9
Arthur Zopellaro <github@zope.me>
2024-07-18 22:02:41 -0300

    feat: add uv

---
+++ scripts/cli/pip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+APPS="uv"
+
+APP_NAME="pip"
+LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
+echo ">> Logging to $LOG_TMP_FILE"
+date >> $LOG_TMP_FILE
+
+echo ">> Installing $APPS..." | tee -a $LOG_TMP_FILE
+pip install --break-system-packages --upgrade $APPS >> $LOG_TMP_FILE
+
