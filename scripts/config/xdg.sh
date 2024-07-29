29dc0c23a1bb471fd460f7b4cac4e9d90eff634d
Arthur Zopellaro <github@zope.me>
2024-07-29 14:40:52 -0300

    feat: add config folder and scripts

---
+++ scripts/config/xdg.sh
@@ -0,0 +1,15 @@
+APP_NAME="xdg"
+CONFIG_FILENAME="user-dirs.dirs"
+CONFIG_DOTFILE_DIR="../../config"
+CONFIG_HOME_DIR="$HOME/.config"
+CONFIG_IN="$CONFIG_DOTFILE_DIR/${APP_NAME}/$CONFIG_FILENAME"
+CONFIG_OUT="$CONFIG_HOME_DIR/$CONFIG_FILENAME"
+
+LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
+echo ">> Logging to $LOG_TMP_FILE"
+date >> $LOG_TMP_FILE
+
+echo ">> Configuring $APP_NAME..." | tee -a $LOG_TMP_FILE
+cp -v $CONFIG_IN $CONFIG_OUT >> $LOG_TMP_FILE
+echo ">> Updating $APP_NAME..." | tee -a $LOG_TMP_FILE
+xdg-user-dirs-update
