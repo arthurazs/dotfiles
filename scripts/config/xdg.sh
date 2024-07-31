6cde41c4a4aa9743b5533909258ca779ac70c07d
Arthur Zopellaro <github@zope.me>
2024-07-31 18:08:04 +0000

    fix: xdg

--- scripts/config/xdg.sh
+++ scripts/config/xdg.sh
@@ -1,3 +1,5 @@
+NEW_DIRS="Desktop Templates Public Music Videos"
+
 APP_NAME="xdg"
 CONFIG_FILENAME="user-dirs.dirs"
 CONFIG_DOTFILE_DIR="../../config"
@@ -9,7 +11,12 @@ LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
 echo ">> Logging to $LOG_TMP_FILE"
 date >> $LOG_TMP_FILE
 
+echo ">> Creating dirs for $NEW_DIRS..." | tee -a $LOG_TMP_FILE
+echo "$NEW_DIRS" | tr ' ' '\n' | while read NEW_DIR; do
+    mkdir -p $HOME/.$APP_NAME/$NEW_DIR
+done
+
 echo ">> Configuring $APP_NAME..." | tee -a $LOG_TMP_FILE
 cp -v $CONFIG_IN $CONFIG_OUT >> $LOG_TMP_FILE
 echo ">> Updating $APP_NAME..." | tee -a $LOG_TMP_FILE
-xdg-user-dirs-update
+xdg-user-dirs-update >> $LOG_TMP_FILE
