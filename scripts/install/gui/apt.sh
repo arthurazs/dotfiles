270f14fe5037abde00552806ae419d25791104fe
Arthur Zopellaro <github@zope.me>
2024-07-31 18:46:20 -0300

    feat: add gnome-browser-connector

--- scripts/install/gui/apt.sh
+++ scripts/install/gui/apt.sh
@@ -1,7 +1,6 @@
-
 #!/bin/bash
 
-APPS="alacritty gimp"
+APPS="alacritty gimp gnome-browser-connector"
 
 APP_NAME="apt.gui"
 LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
@@ -12,5 +11,3 @@ echo ">> Updating $APP_NAME..." | tee -a $LOG_TMP_FILE
 sudo apt-get update >> $LOG_TMP_FILE
 echo ">> Installing $APPS..." | tee -a $LOG_TMP_FILE
 sudo apt-get install $APPS -y >> $LOG_TMP_FILE
-
-
