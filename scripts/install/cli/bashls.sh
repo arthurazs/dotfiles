df8283d42d74767d788ac4114f98795d053b52cb
Arthur Zopellaro <github@zope.me>
2024-08-07 16:50:28 -0300

    feat: add bashls

---
+++ scripts/install/cli/bashls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+APP_NAME="bash-language-server"
+LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
+echo ">> Logging to $LOG_TMP_FILE"
+date >> $LOG_TMP_FILE
+
+echo ">> Installing $APP_NAME lastest version..." | tee -a $LOG_TMP_FILE
+sudo snap install bash-language-server --classic >> $LOG_TMP_FILE
