0deb4b115a9ec3711aba821b71cf5f6a21bf8936
Arthur Zopellaro <github@zope.me>
2024-08-08 16:20:06 -0300

    ref: improve rust-analyzer

--- scripts/install/cli/rust-analyzer.sh
+++ scripts/install/cli/rust-analyzer.sh
@@ -3,8 +3,7 @@
 APP_NAME="rust-analyzer"
 LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
 echo ">> Logging to $LOG_TMP_FILE"
-date >> $LOG_TMP_FILE
+date >> "$LOG_TMP_FILE"
 
-echo ">> Installing $APP_NAME..." | tee -a $LOG_TMP_FILE
-$HOME/.local/cargo/bin/rustup component add $APP_NAME
-
+echo ">> Installing $APP_NAME..." | tee -a "$LOG_TMP_FILE"
+rustup component add "$APP_NAME" >> "$LOG_TMP_FILE"
