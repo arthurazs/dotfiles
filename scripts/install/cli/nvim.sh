1f98db3c14609325ced28b8439584836105d427d
Arthur Zopellaro <github@zope.me>
2024-08-14 15:27:09 -0300

    fix: improve nvim install

--- scripts/install/cli/nvim.sh
+++ scripts/install/cli/nvim.sh
@@ -6,25 +6,28 @@ APP_NAME="nvim"
 APP_REPO="neovim/neovim"
 LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
 echo ">> Logging to $LOG_TMP_FILE"
-date >> $LOG_TMP_FILE
+date >>"$LOG_TMP_FILE"
 
 APP_TMP_DIR=$(mktemp -p "/tmp" -d "$APP_NAME.XXXXX")
 APP_EXTENSION="tar.gz"
 APP_TMP_FILE="$APP_TMP_DIR/$APP_NAME.$APP_EXTENSION"
-APP_BASE_DIR="$HOME/.local/bin"
+BASE_DIR="$HOME/.local"
+APP_BASE_DIR="$BASE_DIR/$APP_NAME"
 
-echo ">> Searching for $APP_NAME latest version..." | tee -a $LOG_TMP_FILE
-APP_VERSION=`latest_version $APP_REPO`
+echo ">> Searching for $APP_NAME latest version..." | tee -a "$LOG_TMP_FILE"
+APP_VERSION=$(latest_version $APP_REPO)
 APP_FILENAME="${APP_NAME}-linux64.$APP_EXTENSION"
 
-echo ">> Downloading $APP_NAME version $APP_VERSION..." | tee -a $LOG_TMP_FILE
-wget `version_url $APP_REPO $APP_VERSION $APP_FILENAME` -O $APP_TMP_FILE -a $LOG_TMP_FILE
+echo ">> Downloading $APP_NAME version $APP_VERSION..." | tee -a "$LOG_TMP_FILE"
+wget "$(version_url "$APP_REPO" "$APP_VERSION" "$APP_FILENAME")" -O "$APP_TMP_FILE" -a "$LOG_TMP_FILE"
 
-echo ">> Decompressing $APP_NAME version $APP_VERSION..." | tee -a $LOG_TMP_FILE
-tar -xzf $APP_TMP_FILE -v -C $APP_TMP_DIR >> $LOG_TMP_FILE
-mv -v $APP_TMP_DIR/nvim-linux64/bin/nvim $APP_BASE_DIR >> $LOG_TMP_FILE
-mv -v $APP_TMP_DIR/nvim-linux64/lib/nvim $HOME/.local/lib/nvim >> $LOG_TMP_FILE
-mv -v $APP_TMP_DIR/nvim-linux64/share/nvim/runtime $HOME/.local/share/nvim >> $LOG_TMP_FILE
+echo ">> Decompressing $APP_NAME version $APP_VERSION..." | tee -a "$LOG_TMP_FILE"
+{
+	tar -xzf "$APP_TMP_FILE" -v -C "$APP_TMP_DIR"
+	rm -rfv "$APP_BASE_DIR"
+	mv -v "$APP_TMP_DIR/nvim-linux64" "$APP_BASE_DIR"
+	ln -vsf "$APP_BASE_DIR/bin/$APP_NAME" "$BASE_DIR/bin/$APP_NAME"
+} >>"$LOG_TMP_FILE"
 
-echo ">> Removing tmp dir..." | tee -a $LOG_TMP_FILE
-rm -vrf $APP_TMP_DIR >> $LOG_TMP_FILE
+echo ">> Removing tmp dir..." | tee -a "$LOG_TMP_FILE"
+rm -vrf "$APP_TMP_DIR" >>"$LOG_TMP_FILE"
