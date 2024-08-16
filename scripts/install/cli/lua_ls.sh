b3d59bf42efc618d35ddbbbbb5952243d8b46ed9
Arthur Zopellaro <github@zope.me>
2024-08-16 14:37:58 -0300

    fix: broken lua_ls install

--- scripts/install/cli/lua_ls.sh
+++ scripts/install/cli/lua_ls.sh
@@ -22,14 +22,13 @@ echo ">> Downloading $APP_NAME version $APP_VERSION..." | tee -a "$LOG_TMP_FILE"
 wget "$(version_url "$APP_REPO" "$APP_VERSION" "$APP_FILENAME")" -O "$APP_TMP_FILE" -a "$LOG_TMP_FILE"
 
 echo ">> Decompressing $APP_NAME version $APP_VERSION..." | tee -a "$LOG_TMP_FILE"
-tar -xzf "$APP_TMP_FILE" -v -C "$APP_TMP_DIR" >>"$LOG_TMP_FILE"
-
-echo ">> Installing $APP_NAME version $APP_VERSION..." | tee -a "$LOG_TMP_FILE"
 {
 	mkdir -p -v "$APP_BASE_DIR"
-	mv -v "$APP_TMP_DIR" "$APP_BASE_DIR"
-	ln -vsf "$APP_BASE_DIR/bin/$APP_NAME" "$BASE_DIR/bin/$APP_NAME"
+	tar -xzf "$APP_TMP_FILE" -v -C "$APP_BASE_DIR"
 } >>"$LOG_TMP_FILE"
+
+echo ">> Installing $APP_NAME version $APP_VERSION..." | tee -a "$LOG_TMP_FILE"
+ln -vsf "$APP_BASE_DIR/bin/$APP_NAME" "$BASE_DIR/bin/$APP_NAME" >>"$LOG_TMP_FILE"
 
 echo ">> Removing tmp dir..." | tee -a "$LOG_TMP_FILE"
 rm -vrf "$APP_TMP_DIR" >>"$LOG_TMP_FILE"
