9e732b49bfad2732214aa80cfb467821b240718e
Arthur Zopellaro <github@zope.me>
2024-12-26 18:20:38 -0300

    fix: add -y

--- scripts/install/gui/obsidian.sh
+++ scripts/install/gui/obsidian.sh
@@ -22,7 +22,7 @@ wget "$(version_url "$APP_REPO" "$APP_VERSION" "$APP_FILENAME")" -O "$APP_TMP_FI
 echo ">> Installing $APP_NAME version $APP_VERSION..." | tee -a "$LOG_TMP_FILE"
 {
 	sudo dpkg -i "$APP_TMP_FILE"
-	sudo apt-get install -f
+	sudo apt-get install -f -y
 } >>"$LOG_TMP_FILE"
 
 echo ">> Removing tmp dir..." | tee -a "$LOG_TMP_FILE"
