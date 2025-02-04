cc153bac4173a55590ec77b2d216c3ea7de5f845
Arthur Zopellaro <github@zope.me>
2025-02-04 10:32:18 -0300

    feat: add alacritty and libfuse2, remove git and curl

--- scripts/install/cli/apt.sh
+++ scripts/install/cli/apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APPS="eza bat nala fish python3-pip btop fd-find ripgrep fzf zoxide git curl"
+APPS="eza bat nala fish python3-pip btop fd-find ripgrep fzf zoxide alacritty libfuse2"
 
 APP_NAME="apt.cli"
 LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
