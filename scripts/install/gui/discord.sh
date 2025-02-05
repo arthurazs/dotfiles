795fd1f055160d2b02c1b563348d1dd213ce794f
Arthur Zopellaro <github@zope.me>
2025-02-05 20:04:59 -0300

    fix: improve discord sh

--- scripts/install/gui/discord.sh
+++ scripts/install/gui/discord.sh
@@ -1,24 +1,26 @@
-#!/bin/bash
+#!/usr/bin/sh
 
-. ./../clean_exit.sh
+PARENT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+# shellcheck source=../trap.sh
+. "${PARENT_DIR}/trap.sh"
+# shellcheck source=../log.sh
+. "${PARENT_DIR}/log.sh"
 
 APP_NAME="discord"
-APP_URL="https://discord.com/api/download?platform=linux&format=deb"
+APP_URL=""
 LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
 echo ">> Logging to $LOG_TMP_FILE"
-date >>"$LOG_TMP_FILE"
+log2file date
 
 APP_TMP_DIR=$(mktemp -p "/tmp" -d "$APP_NAME.XXXXX")
 APP_TMP_FILE="$APP_TMP_DIR/$APP_NAME.deb"
 
-echo ">> Downloading $APP_NAME latest version..." | tee -a "$LOG_TMP_FILE"
-wget "$APP_URL" -O "$APP_TMP_FILE" -a "$LOG_TMP_FILE"
+echo ">> Downloading $APP_NAME latest version..."
+log2file curl -fsSL "https://discord.com/api/download?platform=linux&format=deb" -o "$APP_TMP_FILE"
 
-echo ">> Installing $APP_NAME..." | tee -a "$LOG_TMP_FILE"
-{
-	sudo dpkg -i "$APP_TMP_FILE"
-	sudo apt-get install -f -y
-} >>"$LOG_TMP_FILE"
+echo ">> Installing $APP_NAME..."
+	log2file sudo dpkg -i "$APP_TMP_FILE"
+	log2file sudo apt-get install -f -y
 
-echo ">> Removing tmp dir..." | tee -a "$LOG_TMP_FILE"
-rm -vrf "$APP_TMP_DIR" >>"$LOG_TMP_FILE"
+echo ">> Removing tmp dir..."
+log2file rm -vrf "$APP_TMP_DIR"
