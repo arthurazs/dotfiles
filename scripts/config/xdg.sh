b0561c2816905ce0e6bb29082bc210904cd3710f
Arthur Zopellaro <github@zope.me>
2025-04-07 16:00:49 -0300

    fix: use trap and log helper scripts

--- scripts/config/xdg.sh
+++ scripts/config/xdg.sh
@@ -1,22 +1,28 @@
+#!/usr/bin/sh
+APP_NAME="xdg"
+PARENT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+ROOT_DIR="$(dirname ${PARENT_DIR})"
+# shellcheck source=../trap.sh
+. "${PARENT_DIR}/trap.sh"
+# shellcheck source=../log.sh
+. "${PARENT_DIR}/log.sh"
+
 NEW_DIRS="Desktop Templates Public Music Videos"
 
-APP_NAME="xdg"
 CONFIG_FILENAME="user-dirs.dirs"
-CONFIG_DOTFILE_DIR="../../config"
+CONFIG_DOTFILE_DIR="${ROOT_DIR}/config"
 CONFIG_HOME_DIR="$HOME/.config"
 CONFIG_IN="$CONFIG_DOTFILE_DIR/${APP_NAME}/$CONFIG_FILENAME"
 CONFIG_OUT="$CONFIG_HOME_DIR/$CONFIG_FILENAME"
 
 LOG_TMP_FILE=$(mktemp -p "/tmp" "$APP_NAME.XXXXX.log")
-echo ">> Logging to $LOG_TMP_FILE"
-date >> $LOG_TMP_FILE
+echo ">> Logging to ${LOG_TMP_FILE}"
+log2file date
 
-echo ">> Creating dirs for $NEW_DIRS..." | tee -a $LOG_TMP_FILE
-echo "$NEW_DIRS" | tr ' ' '\n' | while read NEW_DIR; do
-    mkdir -p $HOME/.$APP_NAME/$NEW_DIR
-done
+echo ">> Creating dirs for ${NEW_DIRS}..."
+log2file echo "$NEW_DIRS" | tr ' ' '\n' | while read NEW_DIR; do mkdir -p $HOME/.$APP_NAME/$NEW_DIR; done
 
-echo ">> Configuring $APP_NAME..." | tee -a $LOG_TMP_FILE
-cp -v $CONFIG_IN $CONFIG_OUT >> $LOG_TMP_FILE
-echo ">> Updating $APP_NAME..." | tee -a $LOG_TMP_FILE
-xdg-user-dirs-update >> $LOG_TMP_FILE
+echo ">> Configuring ${APP_NAME}..."
+log2file cp -v $CONFIG_IN $CONFIG_OUT
+echo ">> Updating ${APP_NAME}..."
+log2file xdg-user-dirs-update
